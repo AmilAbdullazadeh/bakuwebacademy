05ec331f1465e424336443bd33a7b0448508bb35
Amil Abdullazadeh <amilva@code.edu.az>
2021-02-18 11:55:56 +0400

    test 1

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import sanityClient from "../client";
+import image from '../assets/bg-img.jpg';
+import imageUrlBuilder from '@sanity/image-url';
+import BlockContent from "@sanity/block-content-to-react";
+
+const builder = imageUrlBuilder(sanityClient)
+
+function urlFor(source) {
+    return builder.image(source)
+}
 
 export default function About(props) {
+
+    const [author, setAuthor] = useState(null)
+
+    useEffect(() => {
+        sanityClient.fetch(`*[_type == "author"]{
+            name,
+            bio,
+            slug,
+            "authorImage": image.asset->url
+        }`)
+            .then((data) => setAuthor(data[0]))
+            .catch(console.error)
+    }, [author])
+
+    if (!author) return <div>Loading...</div>
+
     return (
-        <h1>sd</h1>
+        <div>
+            <main className="main-about">
+                <img src={image} />
+                <div>
+                    <section className="section-about">
+                        <img src={urlFor(author.authorImage).url()} />
+                        <div>
+                            <h1>
+                                Hey there. Our us "{ }"
+                                <span>{author.name}</span>
+                            </h1>
+                            <div>
+                                <BlockContent blocks={author.bio} projectId='nhn0majf' dataset='production' />
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </main>
+        </div>
     )
 }
